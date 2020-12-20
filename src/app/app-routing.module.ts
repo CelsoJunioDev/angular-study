f48acf8e2c0c37a3cd4c51fb64ca137f99e2db6c
CelsoJunioDev <celsoj18@gmail.com>
2020-12-20 20:30:05 -0300

    routes lazyLoad generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import {MenuComponent} from './menu/menu.component'
+import { UsuarioComponent } from './usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'menu', component: MenuComponent},
+  {path: 'footer', component: FooterComponent},
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(h => h.HomeModule)
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then(u => u.UsuarioModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
